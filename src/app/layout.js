07edd565e1eddf23ee82616125b14a10ba314911
shakogegia <shalva.gegia@gmail.com>
2023-10-30 21:03:49 +0100

    fix: google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ImagesProvider } from '@/providers/images-provider'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YF91R7D90K"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-1KJJ787RMR');
+            `}
+        </Script>
         <ImagesProvider>{children}</ImagesProvider>
       </body>
     </html>
