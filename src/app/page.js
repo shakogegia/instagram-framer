14b1f26fa59a2da2ccf86f6beff5507cb0d059ae
shakogegia <shalva.gegia@gmail.com>
2023-10-30 16:11:20 +0100

    feat: kofi

--- src/app/page.js
+++ src/app/page.js
@@ -99,8 +99,26 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className="text-xs font-normal text-neutral-300 text-center mt-10">
-        © 2023 Instagram Framer
+      <footer className="flex flex-col items-center gap-2 text-xs font-normal text-neutral-300 text-center mt-10">
+        <p>© 2023 Instagram Framer</p>
+        <p>
+          If you like this tool, please consider buying me a film. <br /> It
+          will help me to keep this tool up and running.
+        </p>
+        <a
+          href="https://ko-fi.com/V7V8DOK22"
+          target="_blank"
+          rel="noreferrer"
+          className="mt-2"
+        >
+          <img
+            height="36"
+            style={{ border: '0px', height: '36px' }}
+            src="https://storage.ko-fi.com/cdn/kofi5.png?v=3"
+            border="0"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
       </footer>
     </main>
   )
